1a1904d117c80e2923c887992c98032f120616e1
clickar <clickar14@gmail.com>
2019-03-03 10:52:17 -0800

    Uploading Base Program

---
+++ RideStalk/RideStalk/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Firebase.Database;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace RideStalk
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Interface());
+        }
+    }
+}
