78acb48ecee93c0bd050d2796e32b65b5277c0ee
clickar <clickar14@gmail.com>
2019-03-23 23:17:28 -0700

    Introduced car data
    Edited the car data file, which contains all the classes that will be referenced within the database.

--- RideStalk/RideStalk/CarData.cs
+++ RideStalk/RideStalk/CarData.cs
@@ -3,11 +3,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Firebase.Database.Extensions;
+using Firebase.Database.Offline;
+using Firebase.Database.Query;
+using Firebase.Database.Streaming;
+using Newtonsoft.Json;
 
 namespace RideStalk
 {
-    class CarData
+    public class serviceData
     {
+        public object user { get; set; }
+        public object driver { get; set; }
+        public object carPosition { get; set; }
+        public object destination { get; set; }
+        public object origin { get; set; }
+        public object pointList { get; set; }
+        public string initialTime { get; set; }
+        public string acepted { get; set; }
+        public float travelDistance { get; set; }
+        public int travelTime { get; set; }
+        public string payMode { get; set; }
+        public int pickupDurationTime { get; set; }
+        public string estimatedPrice { get; set; }
+        public string finalPrice { get; set; }
+    }
+    public class user
+    {
+        public string username { get; set; }
+        public int uid { get; set; }
+        public string image { get; set; }
+        public string userCellphone { get; set; }
+        public string userStar { get; set; }
+    }
+    public class driver
+    {
+        public string Company { get; set; }
+        public int did { get; set; }
+        public string image { get; set; }
+        public object car { get; set; }
 
+    }
+    public class car
+    {
+        public string carPlate { get; set; }
+        public string carStars { get; set; }
+    }
+
+    public class carPosition
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+        [JsonProperty("time")]
+        public ServerTimeStamp TimestampPlaceholder { get; } = new ServerTimeStamp();
+    }
+    public class destination
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+        public string destinationName { get; set; }
+        [JsonProperty("time")]
+        public ServerTimeStamp TimestampPlaceholder { get; } = new ServerTimeStamp();
+    }
+    public class origin
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+        public string originName { get; set; }
+        [JsonProperty("time")]
+        public ServerTimeStamp TimestampPlaceholder { get; } = new ServerTimeStamp();
+    }
+    public class pointList
+    {
+        public object point { set; get; }
+    }
+    public class point
+    {
+        public string lat { set; get; }
+        public string lng { set; get; }
+        [JsonProperty("time")]
+        public ServerTimeStamp TimestampPlaceholder { get; } = new ServerTimeStamp();
+    }
+
+    public class ServerTimeStamp
+    {
+        [JsonProperty(".sv")]
+        public string TimestampPlaceholder { get; } = "timestamp";
     }
 }
--- RideStalk/RideStalk/Interface.Designer.cs
+++ RideStalk/RideStalk/Interface.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.splitter1 = new System.Windows.Forms.Splitter();
             this.metroStyleManager1 = new MetroFramework.Components.MetroStyleManager(this.components);
             this.navigationTabs = new MetroFramework.Controls.MetroTabControl();
             this.mapInterface = new System.Windows.Forms.TabPage();
@@ -39,14 +38,6 @@
             this.navigationTabs.SuspendLayout();
             this.mapInterface.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // splitter1
-            // 
-            this.splitter1.Location = new System.Drawing.Point(20, 60);
-            this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(1069, 571);
-            this.splitter1.TabIndex = 5;
-            this.splitter1.TabStop = false;
             // 
             // metroStyleManager1
             // 
@@ -64,10 +55,10 @@
             this.navigationTabs.Cursor = System.Windows.Forms.Cursors.Hand;
             this.navigationTabs.FontSize = MetroFramework.MetroTabControlSize.Tall;
             this.navigationTabs.ItemSize = new System.Drawing.Size(100, 40);
-            this.navigationTabs.Location = new System.Drawing.Point(20, 60);
+            this.navigationTabs.Location = new System.Drawing.Point(23, 63);
             this.navigationTabs.Name = "navigationTabs";
             this.navigationTabs.SelectedIndex = 0;
-            this.navigationTabs.Size = new System.Drawing.Size(1069, 571);
+            this.navigationTabs.Size = new System.Drawing.Size(1069, 575);
             this.navigationTabs.TabIndex = 10;
             this.navigationTabs.UseSelectable = true;
             this.navigationTabs.UseStyleColors = true;
@@ -79,7 +70,7 @@
             this.mapInterface.Font = new System.Drawing.Font("MingLiU-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.mapInterface.Location = new System.Drawing.Point(4, 44);
             this.mapInterface.Name = "mapInterface";
-            this.mapInterface.Size = new System.Drawing.Size(1061, 523);
+            this.mapInterface.Size = new System.Drawing.Size(1061, 527);
             this.mapInterface.TabIndex = 0;
             this.mapInterface.Text = "Map View    ";
             // 
@@ -89,10 +80,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.mapView.AutoScroll = true;
-            this.mapView.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.mapView.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
             this.mapView.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.mapView.Bearing = 0F;
-            this.mapView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.mapView.CanDragMap = false;
             this.mapView.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.mapView.EmptyTileColor = System.Drawing.Color.Navy;
@@ -114,16 +104,17 @@
             this.mapView.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.mapView.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.mapView.ShowTileGridLines = false;
-            this.mapView.Size = new System.Drawing.Size(1061, 533);
+            this.mapView.Size = new System.Drawing.Size(1061, 531);
             this.mapView.TabIndex = 10;
             this.mapView.Zoom = 0D;
             // 
             // mainInterface
             // 
+            this.mainInterface.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.mainInterface.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.mainInterface.Location = new System.Drawing.Point(4, 44);
             this.mainInterface.Name = "mainInterface";
-            this.mainInterface.Size = new System.Drawing.Size(1061, 523);
+            this.mainInterface.Size = new System.Drawing.Size(1061, 507);
             this.mainInterface.TabIndex = 1;
             this.mainInterface.Text = "Trip Summaries";
             // 
@@ -133,7 +124,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1112, 651);
             this.Controls.Add(this.navigationTabs);
-            this.Controls.Add(this.splitter1);
             this.MinimumSize = new System.Drawing.Size(1112, 651);
             this.Name = "Interface";
             this.Style = MetroFramework.MetroColorStyle.Purple;
@@ -145,11 +135,9 @@
             this.mapInterface.ResumeLayout(false);
             this.ResumeLayout(false);
 
-
         }
 
         #endregion
-        private System.Windows.Forms.Splitter splitter1;
         private MetroFramework.Components.MetroStyleManager metroStyleManager1;
         private MetroFramework.Controls.MetroTabControl navigationTabs;
         private System.Windows.Forms.TabPage mapInterface;
