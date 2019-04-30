abab55c969fb2e615b8642fa1fc137fde12ee0b2
Kaylan <Kaylan@DESKTOP-D9B7PEK>
2019-04-30 13:22:32 -0700

    added interaction with Firebase database for storage of valid logins

--- RideStalk/RideStalk/LoginForm.cs
+++ RideStalk/RideStalk/LoginForm.cs
@@ -8,6 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Firebase.Database;
+using Newtonsoft.Json;
+using System.Reactive.Linq;
+using Firebase.Database.Query;
+
 namespace RideStalk
 {
     public partial class LoginForm : MetroFramework.Forms.MetroForm
@@ -25,6 +30,7 @@ namespace RideStalk
             this.StyleManager = metroStyleManager1;
         }
 
+        // returns the value of valid
         public bool is_valid()
         {
             return valid;
@@ -36,20 +42,27 @@ namespace RideStalk
         }
 
         // login button on login panel
-        private void loginButton_Click_1(object sender, EventArgs e)
+        private async void loginButton_Click_1(object sender, EventArgs e)
         {
-            if (usernameField.Text == "admin" && passwordField.Text == "password")
+            // connect to the Firebase database
+            var client = new FirebaseClient("https://test-a6194.firebaseio.com/");
+
+            var logins = await client
+              .Child(usernameField.Text)
+              .OrderByKey()
+              .OnceAsync<Data>();
+            foreach (var login in logins)
             {
-                // treated as valid login
-                valid = true;
-                // close the current window and move to the interface
-                this.Close();
+                if (usernameField.Text == login.Object.name && passwordField.Text == login.Object.password)
+                {
+                    // valid login
+                    valid = true;
+                    // close the current window and move to the interface
+                    this.Close();
+                }
             }
-            else
-            {
-                // make register panel visible if login is invalid
-                registerPanal.Visible = true;
-            }
+            // make register panel visible if login is invalid
+            registerPanal.Visible = true;
         }
 
         // register button on register panel
@@ -65,6 +78,22 @@ namespace RideStalk
             {
                 // treated as valid login
                 valid = true;
+
+                // connect to the Firebase database
+                var client = new FirebaseClient("https://test-a6194.firebaseio.com/");  
+
+                // create new child with the username entered    
+                var child = client.Child(nameEntry.Text);
+
+                // post new login on Firebase database
+                child.PostAsync(new OutboundMessage {
+                    name = nameEntry.Text,
+                    password = passwordEntry.Text,
+                    email = emailEntry.Text,
+                    phone = PhoneEntry.Text,
+                    address = addressEntry.Text,
+                    url = urlEntry.Text
+                });
                 // close the current window and move to the interface
                 this.Close();
             }
@@ -77,4 +106,30 @@ namespace RideStalk
             registerPanal.Visible = false;
         }
     }
+
+    // timestamp for Firebase database
+    public class ServerTimeStamp
+    {
+        [JsonProperty(".sv")]
+        public string TimestampPlaceholder { get; } = "timestamp";
+    }
+
+    // store login data
+    public class Data
+    {
+        public string name { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string url { get; set; }
+    }
+
+    // message for posting to Firebase database
+    public class OutboundMessage : Data
+    {
+        [JsonProperty("Timestamp")]
+        public ServerTimeStamp TimestampPlaceholder { get; } = new ServerTimeStamp();
+    }
+
 }
